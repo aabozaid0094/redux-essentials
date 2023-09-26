5f2df8940d0c1a5625728b66436880d5fc08e108
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-09-26 21:29:27 +0300

    Engage date in all post pages

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,9 +1,9 @@
-import { createSlice } from "@reduxjs/toolkit"
-import { nanoid } from '@reduxjs/toolkit'
+import { createSlice, nanoid } from "@reduxjs/toolkit"
+import { sub } from 'date-fns'
 
 const initialState = [
-    { id: '1', title: 'First Post!', content: 'Hello!' },
-    { id: '2', title: 'Second Post', content: 'More text' }
+    { id: '1', title: 'First Post!', content: 'Hello!', user: '', date: sub(new Date(), { minutes: 10 }).toISOString() },
+    { id: '2', title: 'Second Post', content: 'More text', user: '', date: sub(new Date(), { minutes: 5 }).toISOString() }
 ]
 
 const postsSlice = createSlice({
@@ -18,6 +18,7 @@ const postsSlice = createSlice({
             return {
               payload: {
                 id: nanoid(),
+                date: new Date().toISOString(),
                 title,
                 content,
                 user: userId
