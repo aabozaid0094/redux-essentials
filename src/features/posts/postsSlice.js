c5d158cc1846888077f77d89573b4c5b0c08ea13
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-09-28 13:37:26 +0300

    Engage thunks and use fake store to get posts and users

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,14 +1,20 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit"
-import { sub } from 'date-fns'
+import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
+// import { sub } from 'date-fns'
 
 const initialState = {
   items: [
-    { id: '1', title: 'First Post!', content: 'Hello!', user: '', date: sub(new Date(), { minutes: 10 }).toISOString(), reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 } },
-    { id: '2', title: 'Second Post', content: 'More text', user: '', date: sub(new Date(), { minutes: 5 }).toISOString(), reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 } }
+    // { id: '1', title: 'First Post!', content: 'Hello!', user: '', date: sub(new Date(), { minutes: 10 }).toISOString(), reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 } },
+    // { id: '2', title: 'Second Post', content: 'More text', user: '', date: sub(new Date(), { minutes: 5 }).toISOString(), reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 } }
   ],
   status: 'idle',
   error: null
 }
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+  const response = await client.get('/fakeApi/posts')
+  return response.data
+})
 
 const postsSlice = createSlice({
   name: "posts",
@@ -47,6 +53,21 @@ const postsSlice = createSlice({
         existingPost.user = userId
       }
     }
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = 'loading'
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = 'succeeded'
+        // Add any fetched posts to the array
+        state.items = state.items.concat(action.payload)
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = 'failed'
+        state.error = action.error.message
+      })
   }
 });
 
